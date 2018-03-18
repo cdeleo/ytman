20b0b11daea248a23316f1a23af1ca75f4d67f14
Carter De Leo <carterdeleo@gmail.com>
2018-03-18 18:29:53 +0900

    Add console client id for images datastore.

--- datastore/images_api.py
+++ datastore/images_api.py
@@ -8,8 +8,9 @@ from protorpc import remote
 
 import images
 
+CONSOLE_CLIENT_ID = '955262123852-j4nv7slhidt5lcjve721nj9lo8otmfq5.apps.googleusercontent.com'
 WEB_CLIENT_ID = '955262123852-c1gthms5mhs36q6njvg6kgqu4f1b09q7.apps.googleusercontent.com'
-CLIENT_IDS = [WEB_CLIENT_ID]
+CLIENT_IDS = [CONSOLE_CLIENT_ID, WEB_CLIENT_ID]
 
 # Common messages
 
